950419e35e81ce8e46dcbe420469cc2e7b651dee
modokz <103256716+modokz@users.noreply.github.com>
2022-04-11 04:45:03 +0600

    Update script.js

--- script.js
+++ script.js
@@ -4,12 +4,33 @@ const searchInput = document.querySelector("[data-search]")
 
 let schools = []
 
+function removeElem(arr, value) {
+  const index = arr.indexOf(value)
+  if (index > -1) {
+    arr.splice(index, 1)
+  }
+  return arr
+}
+
+function isContains(words, keys) {
+  initialLength = words.length
+  for (let i = 0; i < keys.length; i++) {
+    for (let j = 0; j < words.length; j++) {
+      if (words[j].includes(keys[i])) {
+        words = removeElem(words, words[j])
+        break
+      }
+    }
+  }
+  return initialLength - keys.length == words.length
+}
+
 searchInput.addEventListener("input", e => {
   const value = e.target.value.toLowerCase()
   schools.forEach(school => {
     const isVisible =
-      school.school_name.toLowerCase().includes(value) ||
-	  school.bin.startsWith(value) 
+      isContains(school.school_name.trim().toLowerCase().split(" "), value.trim().toLowerCase().split(" ")) ||
+      school.bin.startsWith(value)
     school.element.classList.toggle("hide", !isVisible)
   })
 })
@@ -21,9 +42,18 @@ fetch("https://modokz.github.io/data.json")
       const card = schoolCardTemplate.content.cloneNode(true).children[0]
       const header = card.querySelector("[data-header]")
       const body = card.querySelector("[data-body]")
-	  header.textContent = `${school.day} октября, поток ${school.number_of_flow}`
-	  body.textContent = `${school.school_name}, БИН ${school.bin}, ${school.region}, ${school.district}, учащихся в потоке: ${school.students_in_flow}`
+      header.textContent = `${school.day} октября, поток ${school.number_of_flow}`
+      body.textContent = `${school.school_name}, БИН ${school.bin}, ${school.region}, ${school.district}, учащихся в потоке: ${school.students_in_flow}`
       schoolCardContainer.append(card)
-      return { day: school.day, school_name: school.school_name, number_of_flow: school.number_of_flow, bin: school.bin, region: school.region, district: school.district, students_in_flow: school.students_in_flow, element: card }
+      return {
+        day: school.day,
+        school_name: school.school_name,
+        number_of_flow: school.number_of_flow,
+        bin: school.bin,
+        region: school.region,
+        district: school.district,
+        students_in_flow: school.students_in_flow,
+        element: card
+      }
     })
   })
